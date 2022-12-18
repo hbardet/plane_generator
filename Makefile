dfd246ce03c483aca63bfb08472e69e24e56f314
Hugo Bardet <hugo.bardet@epitech.eu>
2022-12-18 17:03:11 +0100

    add ; in print help

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@
 ##
 
 SRC		=	main.c \
+			help_print.c \
 
 OBJ 	= 	$(SRC:.c=.o)
 
---
+++ help_print.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2022
+** B-PSU-100-MLH-1-1-sokoban-hugo.bardet
+** File description:
+** help_print
+*/
+
+#include "radar.h"
+
+int print_help(char *check)
+{
+    if (strcmp(check, "-h") == 0){
+        printf("USAGE\n");
+        printf("\t./plane_gene nb_plane nb_tower\n");
+        printf("DESCRIPTION\n");
+        printf("generate a script with n plane place random coordinate which go to random coordinate\n");
+        printf("");
+        exit(0);
+    } else
+        return (0);
+}
